acf0023163f5de8d7b0e4ce9885130280a7b0ccc
HideOnCodec <tlsdmsgp33@naver.com>
2024-05-09 13:28:30 +0900

    젠킨스 빌드 TEST

---
+++ src/main/java/com/example/testdocker/TestController.java
@@ -0,0 +1,15 @@
+package com.example.testdocker;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "ok";
+    }
+}
